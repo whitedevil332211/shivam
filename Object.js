3a7702b4fb2dcc2f9116b28415dd3be966934b1e
whitedevil332211 <114275690+whitedevil332211@users.noreply.github.com>
2023-03-07 20:24:59 +0530

    Create Object.js
    this is a file who will create an object for you

---
+++ Object.js
@@ -0,0 +1,14 @@
+<html>
+  <body>
+  <h2>JavaScript Objects</h2>
+<p>create an object</p>
+<p id="demo"></p>
+<script>
+  let person = {
+    firstname: john,
+    lastname: deo,
+    age:80,
+    gender:hizra
+  };
+document.getElementById("demo").innerHTML=person.firstName + " " + person.lastName;
+  </script>
